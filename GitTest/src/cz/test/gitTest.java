15ce835c1e71ec158c4d00333b80f193202ba50a
MN <novotnym@nbnovotnym1.all.local>
2014-09-30 10:43:29 +0200

    fix2

--- GitTest/src/cz/test/gitTest.java
+++ GitTest/src/cz/test/gitTest.java
@@ -5,7 +5,7 @@ public class gitTest {
 	public static void main(String[] args) {
 
 
-		System.out.println("Git is fun30 TestFix");
+		System.out.println("Git is fun30 TestFix2");
 		System.out.println("new line 30");
 
 
